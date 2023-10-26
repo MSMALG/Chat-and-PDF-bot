2d68e7b1deb645b35f5778edd87f89c0ffd55e05
phertyameen <81179612+phertyameen@users.noreply.github.com>
2023-10-26 08:55:53 -0700

    Update displayer.py
    Made All internal css  an inline css

--- displayer.py
+++ displayer.py
@@ -1,57 +1,57 @@
-css = '''
-<style>
-.chat-message {
-    padding: 1.5rem; 
-    border-radius: 0.5rem; 
-    margin-bottom: 1rem; 
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-}
-.bot {
-    background-color: #FFFFFF;
-    color: #000;
-    border: 1px solid #228A5E;
-}
-.user {
-    background-color: #228A5E;
-    color: #000;
-    flex-direction: row-reverse;
-}
-.user .message{
-    text-align: end;
-}
-.chat-message .avatar img {
-  max-width: 4.9rem;
-  max-height: 4.9rem;
-  border-radius: 50%;
-  object-fit: cover;
-}
-.chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #000;
-}
-</style>
-'''
+# css = '''
+# <style>
+# .chat-message {
+#     padding: 1.5rem; 
+#     border-radius: 0.5rem; 
+#     margin-bottom: 1rem; 
+#     display: flex;
+#     justify-content: space-around;
+#     align-items: center;
+# }
+# .bot {
+#     background-color: #FFFFFF;
+#     color: #000;
+#     border: 1px solid #228A5E;
+# }
+# .user {
+#     background-color: #228A5E;
+#     color: #000;
+#     flex-direction: row-reverse;
+# }
+# .user .message{
+#     text-align: end;
+# }
+# .chat-message .avatar img {
+#   max-width: 4.9rem;
+#   max-height: 4.9rem;
+#   border-radius: 50%;
+#   object-fit: cover;
+# }
+# .chat-message .message {
+#   width: 80%;
+#   padding: 0 1.5rem;
+#   color: #000;
+# }
+# </style>
+# '''
 
 
 bot_template = '''
-<div class="chat-message bot">
+<div class="chat-message bot" style="padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex;justify-content: space-around; align-items: center; background-color: #FFFFFF; color: #000; border: 1px solid #228A5E;">
     <div class="avatar">
         <img src="https://www.clipartmax.com/png/middle/118-1184192_virtual-assistants-and-chat-bots-bot-icon-bot.png" 
-        style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        style="max-height: 4.5rem; max-width: 4.5rem; border-radius: 50%; object-fit: cover;">
     </div>
-    <div class="message">{{MSG}}</div>
+    <div class="message" style="text-align: end; width: 80%; padding: 0 1.5rem; color: #000;">{{MSG}}</div>
 </div>
 '''
 
 user_template = '''
-<div class="chat-message user">
+<div class="chat-message user" style="padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex;justify-content: space-around; align-items: center; background-color: #228A5E; color: #000; flex-direction: row-reverse;">
     <div class="avatar">
         <img src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png"
-        style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        style="max-height: 4.5rem; max-width: 4.5rem; border-radius: 50%; object-fit: cover;">
     </div>    
-    <div class="message">{{MSG}}</div>
+    <div class="message" style="text-align: end; width: 80%; padding: 0 1.5rem; color: #000;">{{MSG}}</div>
 </div>
 '''
